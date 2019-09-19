ecae947d26de3144df44e49b279574189d9d8272
Mike Eling <mike.eling97@gmail.com>
2019-09-19 09:03:09 +0200

    :rocket: Initial deploy

---
+++ precache-manifest.c59fc4a2f839e6015670b9a4c64f7858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542d097fac74489155078a0ab4e4defe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "536a2824cb88aae197a9",
+    "url": "/static/css/main.26c1957d.chunk.css"
+  },
+  {
+    "revision": "69bfb2155812d140da31",
+    "url": "/static/js/2.3052b90e.chunk.js"
+  },
+  {
+    "revision": "536a2824cb88aae197a9",
+    "url": "/static/js/main.132b38a0.chunk.js"
+  },
+  {
+    "revision": "416cc1c79b8c22940a8c",
+    "url": "/static/js/runtime~main.7302273d.js"
+  }
+]);
