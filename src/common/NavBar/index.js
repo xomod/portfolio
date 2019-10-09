671e44322a43150b552cd516882e53171ccb4d3a
xomod <xomod99@gmail.com>
2019-10-09 22:31:28 +0200

    :lipstick: NavBar Update

--- src/common/NavBar/index.js
+++ src/common/NavBar/index.js
@@ -4,7 +4,17 @@ import useStyles from "./styles";
 const NavBar = () => {
   const classes = useStyles()
 
-  return <nav>Navbar</nav>
+  return <nav className={classes.navbar}>
+          <a href="index.html"clasName={classes.logo}>Damian Błochowiak</a>
+          <ul>
+             <li>CONTACT</li>     
+          </ul>
+  
+  
+  
+  
+  
+  </nav>
 }
 
 export default NavBar;
--- src/common/NavBar/styles.js
+++ src/common/NavBar/styles.js
@@ -2,6 +2,8 @@ import { createUseStyles } from 'react-jss'
 
 export default createUseStyles(theme => ({
   "navbar": {
-    "background-color": "FFFFFF",
+    "background-color": "#FFFFFF",
+    "padding": "20px",
+
   }
 }))
